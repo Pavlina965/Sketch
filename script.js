547b872cc1ac6c0abcd416d804135ad5e7cf71c7
Pavlina <pavlina965@gmail.com>
2022-07-19 14:17:14 +0200

    sketch - changable grid, clear button

--- script.js
+++ script.js
@@ -1,36 +1,70 @@
-//appending element with id and class
-function createElement(type, className,id, where) {
-    const newDiv = document.createElement(type)
-    //newDiv.className = className
-    newDiv.setAttribute("class",className)
-    newDiv.setAttribute("id",id)
-    newDiv.addEventListener('mouseover', draw)
-    newDiv.addEventListener('mousedown',draw)
-    document.getElementById(where).appendChild(newDiv)
-};
-//creating grid
-let sizeOfGrid = 16 * 16;
-for (let i = 0; i < sizeOfGrid; i++) {
-    createElement('div', 'Grid-elements',i,'grid');
-
-}
-//const boxes = document.querySelectorAll('.Grid-elements');
-//boxes.forEach(el => el.addEventListener('mousedown', event=>{event.target.className= "Colored"}));
+// assignment of elements
+//const slider = document.getElementById('gridSizeSlider');
+const defaultSize = 16;
+const range = document.getElementById('gridSize');
+const grid = document.getElementById('grid');
+const btnClear = document.getElementById('clrBtn');
+const sizeNew = document.getElementById('gridSizeSlider')
+const btnSizeNew = document.getElementById('gridSizeSet')
+const sliderValue = document.getElementById('sliderValue')
+let size = defaultSize;
 let clicked = false
+
+// assignment of actions
 document.body.onmousedown = () => (clicked = true)
 document.body.onmouseup = () => (clicked = false)
-
-function draw(box){
-    if (box.type=== 'mouseover' && !clicked) return
-    else {
-    box.target.className = "Colored"
-        }
+sizeNew.onchange =() => sliderValue.innerHTML = (sizeNew.value +'x'+sizeNew.value);
+//creating element with id and class and event listener
+function createElement(type, className, id, where) {
+    const newDiv = document.createElement(type)
+    newDiv.setAttribute("class", className)
+    newDiv.setAttribute("id", id)
+    newDiv.addEventListener('mouseover', draw)
+    newDiv.addEventListener('mousedown', draw)
+    document.getElementById(where).appendChild(newDiv)
 }
-//function writeId()
-// event listener
-/*
-for (let i =0; i<sizeOfGrid; i++){
-    let box = document.getElementById(i.toString())
-    box.addEventListener()
-}*/
+
+
+//creating grid
+function createGrid(size) {
+    document.getElementById('grid').style.gridTemplateRows = 'repeat(' + size + ',1fr)'
+    document.getElementById('grid').style.gridTemplateColumns = 'repeat(' + size + ',1fr)'
+    let sizeOfGrid = size * size;
+    for (let i = 0; i < sizeOfGrid; i++) {
+        createElement('div', 'Grid-elements', i, 'grid');
+    }
+
+}
+
+function reload() {
+    clear()
+    createGrid(size)
+}
+
+function clear() {
+    grid.innerHTML = ''
+    createGrid()
+}
+
+//updating grid
+btnSizeNew.onclick = () => {
+    (size = sizeNew.value)
+    reload();
+    console.log(size)
+}
+btnClear.onclick = () => {
+    (size = defaultSize)
+    reload()
+}
+
+//function or drawing
+function draw(box) {
+    if (box.type === 'mouseover' && !clicked) return
+    else {
+        box.target.style.backgroundColor = '#ffffff';
+    }
+}
+
+createGrid(size)
+
 
